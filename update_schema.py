73137909f45cc6a9dc244f1764eb156d22d21d4a
Rahul Ravikumar <rahul@rahulrav.com>
2016-04-24 16:42:25 -0700

    Fix migration utility

--- update_schema.py
+++ update_schema.py
@@ -7,7 +7,7 @@ from google.appengine.ext import deferred
 from model import CrashReport
 from search_model import Search
 
-BATCH_SIZE = 100
+BATCH_SIZE = 25
 
 
 class SchemaUpdater(object):
@@ -21,24 +21,18 @@ class SchemaUpdater(object):
         if cursor:
             query.with_cursor(cursor)
 
-        crash_reports = []
+        crash_reports = list()
         for crash_report in query.fetch(limit=BATCH_SIZE):
-            updated = False
-            if not crash_report.version:
-                crash_report.version = '2'
-                updated = True
-            if not crash_report.state:
-                crash_report.state = 'unresolved'
-                updated = True
-            if updated:
-                crash_reports.append(crash_report)
+            crash_report.version = '2'
+            crash_report.state = 'unresolved'
+            crash_reports.append(crash_report)
 
         if crash_reports:
             updated = len(crash_reports)
             logging.info('Updating %s entities', updated)
             # update
             db.put(crash_reports)
-            Search.add_crash_reports(crash_reports)
+            [Search.add_to_index(crash_report) for crash_report in crash_reports]
             # schedule next request
             deferred.defer(SchemaUpdater.update, cursor=query.cursor())
 
